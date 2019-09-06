5935e309965319227bceff43ac674359f1f9f68d
Arun Ganesan <arunganesan123@gmail.com>
2019-09-06 15:24:54 -0400

    added simple `where are you` logging

--- implementations/python-server/how-was-your-day/main.py
+++ implementations/python-server/how-was-your-day/main.py
@@ -1,8 +1,26 @@
+#! /usr/bin/env python3
+
+class libcarlab ():
+    def __init__ (self):
+        '1'
+    
+    def check_new_info (self):
+        return []
+    
+    def output_new_info (self, name, value):
+        # save it to the outbox file
+        return True
+
 def main():
+    cl = libcarlab()
+    
     # check if there is any new info
+    cl.check_new_info()
+
     # take action if needed
+
     # output info if needed
-    return 1
+    cl.output_new_info('mood', 'happy ¬me')
 
 if __name__ == '__main__':
     # test it with dummy data
--- implementations/react/where-are-you/src/Main.js
+++ implementations/react/where-are-you/src/Main.js
@@ -1,7 +1,58 @@
 import React from 'react';
+import { 
+    Button, 
+    Container,
+    FormControl,
+} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 
-export class Main extends React.Component {
+const style = {
+    input: {
+      marginTop: '25px'  
+    },
+    button: {
+        marginTop: '25px'
+    }
+}
+
+export class Main extends React.Component { 
+    constructor (props) {
+        super(props);
+
+        this.state = {
+           message: ''
+        }
+    }
+
+    componentDidMount () {
+        if (navigator && navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((pos) => {
+                const coords = pos.coords;
+                this.setState({
+                    message: `${coords.latitude}, ${coords.longitude}`
+                })
+            })
+        }
+    }
+    
+    submitData() {
+        this.props.onNewData('location', this.state.message);
+    }
+
     render () {
-        return <>ok</>;
+        // Render a map picker where the user can pick their location
+        // This gets uploaded to the server as "new information"
+        return <Container>
+        
+            <FormControl 
+                style={style.input}
+                aria-label="large" 
+                aria-describedby="inputGroup-sizing-lg"
+                value={this.state.message} 
+                onChange={(evt) => this.setState({ 
+                    message: evt.target.value })} />
+        
+            <Button onClick={() => this.submitData()} style={style.button} size="lg">Submit</Button>    
+        </Container>
     }
 }
