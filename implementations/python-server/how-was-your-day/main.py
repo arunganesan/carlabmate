7f4d5751aa17aa04b8cb58ca10e3f64757e7783d
Arun Ganesan <arunganesan123@gmail.com>
2019-09-06 11:27:52 -0400

    .

---
+++ implementations/python-server/how-was-your-day/main.py
@@ -0,0 +1,9 @@
+def main():
+    # check if there is any new info
+    # take action if needed
+    # output info if needed
+    return 1
+
+if __name__ == '__main__':
+    # test it with dummy data
+    main()
