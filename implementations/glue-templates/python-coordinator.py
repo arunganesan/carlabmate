bce4bbb686b1c65b012b61475d7946f1d3dc9269
Arun Ganesan <arunganesan123@gmail.com>
2019-09-09 10:24:13 -0400

    .

--- implementations/glue-templates/python-coordinator.py
+++ implementations/glue-templates/python-coordinator.py
@@ -6,6 +6,7 @@ import time
 SLEEP_TIME = 60 # wake up every 1 minute
 
 # USER-SPECIFIC CONFIG
+# USERID = ...
 
 SCRIPTS = [
     # list each implementation script
@@ -15,7 +16,7 @@ def main():
     # Periodically cycle through and run each script
     while (True):
         for script in SCRIPTS:
-            subprocess.call(['python3', script])
+            subprocess.call(['python3', script, '--userid', USERID])
         time.sleep(SLEEP_TIME)
 
 if __name__ == '__main__':
