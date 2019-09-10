9b9d606d59b853c7ca6ca4cf2a121be274ba2e36
Arun Ganesan <arunganesan123@gmail.com>
2019-09-10 16:52:30 -0400

    really rough sketch of android-cl file upload

--- implementations/libraries/carlab/libcarlab/src/main/java/edu/umich/carlab/net/PacketHandleService.java
+++ implementations/libraries/carlab/libcarlab/src/main/java/edu/umich/carlab/net/PacketHandleService.java
@@ -4,7 +4,39 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.security.MessageDigest;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+import edu.umich.carlab.Constants;
+import edu.umich.carlab.DataMarshal;
+import edu.umich.carlab.utils.Utilities;
 
 
 /**
@@ -14,7 +46,11 @@ import java.util.Map;
  */
 public class PacketHandleService extends Service {
     public final String TAG = PacketHandleService.class.getName();
+    ScheduledFuture<?> scheduled = null;
     final IBinder mBinder = new PacketHandleService.LocalBinder();
+    ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+
+    Map<String, List<DataMarshal.DataObject>> allData = new HashMap<>();
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
@@ -51,12 +87,19 @@ public class PacketHandleService extends Service {
     /****************************************************************
      * Public functions
      ***************************************************************/
-    public boolean outputNewInfo (String info, Object data) {
+    public void outputNewInfo (String info, DataMarshal.DataObject data) {
         /**
          * Add the data to the internal database
          * Will be uploaded asynchronously
          */
-        return true;
+
+        // Save it locally
+        synchronized (allData) {
+            if (!allData.containsKey(info))
+                allData.put(info, new ArrayList<DataMarshal.DataObject>());
+
+            allData.get(info).add(data);
+        }
     }
 
     public Map<String, Object> checkNewInfo () {
@@ -66,6 +109,113 @@ public class PacketHandleService extends Service {
          */
         return null;
     }
+
+
+    Runnable  uploadRunnable = new Runnable() {
+        public void run() {
+            // Save all data to disk
+            synchronized (allData) {
+                for (String info : allData.keySet())
+                    saveOrUpdateData(info, allData.get(info));
+                allData.clear();
+            }
+
+
+            // Load all data stored in the local file
+            Map<String, List<DataMarshal.DataObject>> storedData = loadAllData();
+
+            // Then for each info, make a call to upload
+            for (String info : storedData.keySet()) {
+                // If upload successful, delete file locally
+                RequestQueue queue = Volley.newRequestQueue(context);
+
+                // Get the list of currently uploaded receipts. Only upload ones that weren't succesfully
+                // uploaded and processed
+
+                JSONObject jsonObject = new JSONObject();
+                try {
+                    jsonObject.put("message", "json-ify the storedData.get(info)");
+                } catch (Exception e) {
+                    Log.e(TAG, "JSON error...");
+                }
+
+                JsonObjectRequest myReq = new JsonObjectRequest(Request.Method.POST,
+                        "http:...1234/upload?uid=...",
+                        jsonObject,
+                        gotListSuccess,
+                        volleyError) {
+                };
+            }
+
+        }
+    };
+
+    Response.Listener<JSONObject> gotListSuccess = new Response.Listener<String>() {
+        @Override
+        public void onResponse(String responseJSON) {
+            try {
+               // get the info name that was uploaded
+               // and the last timestamp for that was uploaded
+               // and delete that from local file
+            } catch (Exception e) {
+                Log.e(TAG, "Error parsing JSON of list of uploaded receipts");
+            }
+        }
+    };
+
+    Response.ErrorListener volleyError = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+        }
+    };
+
+    public void dumpData(List<DataMarshal.DataObject> dataObjects) {
+        try {
+            FileOutputStream fos = new FileOutputStream(saveFile);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(dataObjects);
+            oos.close();
+            fos.close();
+            Log.v(TAG, "Saved data dump");
+            Toast.makeText(context, "Saved file to " + saveFile.getAbsolutePath(), Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to write file");
+        }
+    }
+
+    public List<DataMarshal.DataObject> readData(File ifile) {
+        List<DataMarshal.DataObject> returnData = null;
+
+        try {
+            FileInputStream fis = new FileInputStream(ifile);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            returnData = (List<DataMarshal.DataObject>)ois.readObject();
+            ois.close();
+            fis.close();
+            Log.v(TAG, "Loaded data");
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to write file");
+        }
+
+        return returnData;
+    }
+
+    public void scheduleUploads () {
+        if (scheduled == null) {
+            scheduled = scheduler.scheduleAtFixedRate(
+                    uploadRunnable,
+                    0,
+                    1,
+                    TimeUnit.MINUTES);
+        }
+    }
+
+    public void unscheduleUploads () {
+        if (scheduled != null) {
+            scheduled.cancel(false);
+        }
+    }
+
 
     public class LocalBinder extends Binder {
         public PacketHandleService getService() {
