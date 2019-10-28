87483cbecf1ca9f6119c93d8b56eaaf8b8933f22
Arun Ganesan <arunganesan123@gmail.com>
2019-10-28 17:40:01 -0400

    .

---
+++ algorithms/aligned-imu/sandbox/src/main/java/edu/umich/sandbox/Sandbox.java
@@ -0,0 +1,14 @@
+package edu.umich.sandbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sandbox extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sandbox);
+    }
+}
