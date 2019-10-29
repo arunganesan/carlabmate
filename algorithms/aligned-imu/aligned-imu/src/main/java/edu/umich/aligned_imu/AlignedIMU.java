420d3c87dfd79e11ff8c92bc9930c0bd1503c668
Arun Ganesan <arunganesan123@gmail.com>
2019-10-28 22:36:38 -0400

    .

--- algorithms/aligned-imu/aligned-imu/src/main/java/edu/umich/aligned_imu/AlignedIMU.java
+++ algorithms/aligned-imu/aligned-imu/src/main/java/edu/umich/aligned_imu/AlignedIMU.java
@@ -1,8 +1,82 @@
 package edu.umich.aligned_imu;
 
-public class AlignedIMU {
+import android.content.Context;
+
+import edu.umich.carlab.CLDataProvider;
+import edu.umich.carlab.DataMarshal;
+import edu.umich.carlab.loadable.App;
+import edu.umich.carlab.sensors.PhoneSensors;
+
+public class AlignedIMU extends App {
+    private DataMarshal.DataObject lastMagnet,
+            lastGravity,
+            lastGyro,
+            lastAccel;
+
+    private boolean calculatedRotation = false;
+
+    public AlignedIMU(CLDataProvider cl, Context context) {
+        super(cl, context);
+
+        name = "world_aligned_imu";
+//        middlewareName = MiddlewareImpl.APP;
+        subscribe(PhoneSensors.DEVICE, PhoneSensors.GRAVITY);
+        subscribe(PhoneSensors.DEVICE, PhoneSensors.MAGNET);
+        subscribe(PhoneSensors.DEVICE, PhoneSensors.GYRO);
+        subscribe(PhoneSensors.DEVICE, PhoneSensors.ACCEL);
+    }
+
+
+    @Override
+    public void newData(DataMarshal.DataObject dObject) {
+        super.newData(dObject);
+
+        long time = dObject.time;
+        String sensor = dObject.sensor;
+
+        if (dObject.dataType != DataMarshal.MessageType.DATA) return;
+//        if (dObject.device.equals(MiddlewareImpl.APP)) return;
+        if (dObject.value == null) return;
+
+        switch (sensor) {
+            case PhoneSensors.GRAVITY:
+                lastGravity = dObject;
+                break;
+            case PhoneSensors.MAGNET:
+                lastMagnet = dObject;
+                break;
+            case PhoneSensors.GYRO:
+                lastGyro = dObject;
+                break;
+            case PhoneSensors.ACCEL:
+                lastAccel = dObject;
+                break;
+        }
+
+
+        /*
+        Need to go from the JSON description of the IO of this algorithm to the following functions
+
+        0. Take input and call the function
+        1. Get the return value and save it
+        2. ... Um that's it?
+         */
+
+        if (sensor.equals(PhoneSensors.MAGNET) || sensor.equals(PhoneSensors.GRAVITY)) {
+            if (lastMagnet != null && lastGravity != null) {
+                produceRotation(lastMagnet.value, lastGravity.value);
+                calculatedRotation = true;
+            }
+        } else if (sensor.equals(PhoneSensors.GYRO) && calculatedRotation)
+            produceAlignedGyro(lastGyro.value);
+        else if (sensor.equals(PhoneSensors.ACCEL) && calculatedRotation)
+            produceAlignedGyro(lastAccel.value);
+    }
+
+
+
     float[][] rm = new float[3][3];
-    public float[][] produceRotation (float [] m, float [] g) {
+    public float[][] produceRotation (Float [] m, Float [] g) {
         // Cross product of magnet and gravity
         float[] c = { m[1]*g[2]-m[2]*g[1] , m[2]*g[0]-m[0]*g[2], m[0]*g[1]-m[1]*g[0]};
 
@@ -23,16 +97,16 @@ public class AlignedIMU {
         return rm;
     }
 
-    public float[] produceAlignedGyro (float [] gyro) {
+    public Float[] produceAlignedGyro (Float [] gyro) {
         return MatrixMul(gyro, rm);
     }
 
-    public float[] produceAlignedAccel (float [] accel) {
+    public Float[] produceAlignedAccel (Float [] accel) {
         return MatrixMul(accel, rm);
     }
 
-    public float[] MatrixMul(float[] T, float[][] RotMat) {
-        float[] temp = T.clone();
+    public Float[] MatrixMul(Float[] T, float[][] RotMat) {
+        Float[] temp = T.clone();
         for (int i = 0; i < RotMat.length; i++)
             for (int j = 0; j < T.length; j++) {
                 temp[i] = temp[i] + T[j] * RotMat[j][i];
