fa710d08d0490f084e5eaf5a575d8f5edfcadd0f
Arun Ganesan <arunganesan123@gmail.com>
2019-11-01 21:01:09 -0400

    started wiring and packaging android code

--- helpers/libraries/carlab/libcarlab/src/main/java/edu/umich/carlab/loadable/AlgorithmSpecs.java
+++ helpers/libraries/carlab/libcarlab/src/main/java/edu/umich/carlab/loadable/AlgorithmSpecs.java
@@ -5,13 +5,20 @@ import android.hardware.Sensor;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+
+import edu.umich.carlab.CLService;
+import edu.umich.carlab.sensors.PhoneSensors;
+import edu.umich.carlab.utils.DevSen;
 
 public class AlgorithmSpecs {
     public static Map<String, Object> InformationDatatypes;
     public static Map<String, Integer> LowLevelSensors;
     public static Map<String, String> LowLevelSensorsNames;
+    public static HashSet<String> RawSensors;
+    public static Map<String, DevSen> DevSenMapping;
 
     static {
         InformationDatatypes = new HashMap<>();
@@ -24,17 +31,13 @@ public class AlgorithmSpecs {
         InformationDatatypes.put("magnetometer", new float[3]);
 
         LowLevelSensors = new HashMap<>();
-        LowLevelSensors.put("rotation", Sensor.TYPE_ROTATION_VECTOR);
+        LowLevelSensors.put("rotation", -1);
         LowLevelSensors.put("gravity", Sensor.TYPE_GRAVITY);
         LowLevelSensors.put("world-aligned-gyro", -1);
         LowLevelSensors.put("world-aligned-accel", -1);
         LowLevelSensors.put("accel", Sensor.TYPE_ACCELEROMETER);
         LowLevelSensors.put("gyro", Sensor.TYPE_GYROSCOPE);
         LowLevelSensors.put("magnetometer", Sensor.TYPE_MAGNETIC_FIELD);
-
-
-
-
 
         LowLevelSensorsNames = new HashMap<>();
         LowLevelSensorsNames.put("rotation", Sensor.STRING_TYPE_ROTATION_VECTOR);
@@ -44,6 +47,16 @@ public class AlgorithmSpecs {
         LowLevelSensorsNames.put("accel", Sensor.STRING_TYPE_ACCELEROMETER);
         LowLevelSensorsNames.put("gyro", Sensor.STRING_TYPE_GYROSCOPE);
         LowLevelSensorsNames.put("magnetometer", Sensor.STRING_TYPE_MAGNETIC_FIELD);
+
+        RawSensors = new HashSet<>();
+        RawSensors.addAll(Arrays.asList("gravity", "accel", "gyro", "magnetometer"));
+
+        DevSenMapping = new HashMap<>();
+        DevSenMapping.put("gravity", new DevSen(PhoneSensors.DEVICE, PhoneSensors.GRAVITY));
+        DevSenMapping.put("accel", new DevSen(PhoneSensors.DEVICE, PhoneSensors.ACCEL));
+        DevSenMapping.put("gyro", new DevSen(PhoneSensors.DEVICE, PhoneSensors.GYRO));
+        DevSenMapping.put("magnetometer", new DevSen(PhoneSensors.DEVICE, PhoneSensors.MAGNET));
+
     }
 
     public static class AppFunction {
