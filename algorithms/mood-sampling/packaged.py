bca9111a8b192407044751596da27c201b2e6958
Arun Ganesan <arunganesan123@gmail.com>
2019-11-04 17:00:40 -0500

    python coding

---
+++ algorithms/mood-sampling/packaged.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+
+"""
+This is the wrapper script 
+
+It is responsible for just a few things:
+
+    0. Initialize and give life to algorithms
+    1. Multiplex data around
+    2. IO from gateway server
+"""
+
+def main():
+    # Load algorithms
+    
+
+if __name__ == '__main__':
+    main()
---
+++ algorithms/mood-sampling/sandbox.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+
+"""
+Generated for this specific guy
+It could use library calls belonging to general CarLab
+But fundamentally it just 
+"""
