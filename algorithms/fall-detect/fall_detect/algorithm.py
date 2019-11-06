b6a43885a281ad5f828b6323c462cc670eab3ee3
Arun Ganesan <arunganesan123@gmail.com>
2019-11-06 09:03:57 -0500

    .

--- algorithms/fall-detect/fall_detect/algorithm.py
+++ algorithms/fall-detect/fall_detect/algorithm.py
@@ -1,4 +1,60 @@
-class FallDetect:
+#! /usr/bin/env python3
+
+class AlgorithmFunction:
+    def __init__ (self, name, belongsto, outputinfo, inputinfo):
+        self.name = name
+        self.belongsto = belongsto
+        self.outputinfo = outputinfo
+        self.inputinfo = inputinfo
+        
+
+class Algorithm:
+    def __init__ (self):
+        self.functions = []
+
+    def add_new_data(self, data_marshal):
+        # check the information of the data
+        return 0
+    
+    def output_data(self, data_marshal):
+        if data_marshal.value is None:
+            return
+
+        # otherwise output this data.
+        # using CL library
+        # to output means to save it locally, and then periodically uploading it
+
+class Information:
+    def __init__ (self, name, datatype):
+        self.name = name
+        self.datatype = datatype
+
+# Information statically created. 
+class WorldAlignedAccel (Information):
+    def __init__ (self):
+        self.name = 'world-aligned-accel'
+        self.datatype = [0.0]*3
+
+class Fall (Information):
+    def __init__ (self):
+        self.name = 'fall'
+        self.datatype = True
+
+
+class DataMarshal:
+    def __init__ (self):
+        self.data = 1
+
+
+
+
+
+
+
+
+
+
+class FallDetectBase (Algorithm):
     def __init__ (self):
         import libcarlab
         self.algorithm_functions = [
@@ -6,8 +62,18 @@ class FallDetect:
         ]
         # self.algorithm_functions = [
         #     self.produce_fall, output = "fall", inp = ["world aligned accel"]]
-    
-    
+     
+    def add_new_data(self, data):
+        # call super add_new_data(data)
+        if type(data.info) is WorldAlignedAccel:
+            self.output_data(self.produce_fall(data.value))
+
+    def produce_fall (self, world_aligned_accel):
+        print(world_aligned_accel)
+        return None
+
+   
+class FallDetect (FallDetectBase):
     def produce_fall (self, world_aligned_accel):
         print(world_aligned_accel)
         return None
