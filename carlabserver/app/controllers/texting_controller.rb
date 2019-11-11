d0815449b45d5b1e29ce0d77538710cc940d2765
CarSec Research <arun.ganesan.123@gmail.com>
2019-11-11 12:00:51 -0500

    twilio text response simple stub

--- carlabserver/app/controllers/texting_controller.rb
+++ carlabserver/app/controllers/texting_controller.rb
@@ -3,15 +3,17 @@ class TextingController < ApplicationController
     require 'fileutils'
     require 'twilio-ruby'
 
-    account_sid = ENV['TWILIO_ACCOUNT_SID']
-    auth_token = ENV['TWILIO_AUTH_TOKEN']
-    @client = Twilio::REST::Client.new(account_sid, auth_token)
 
     skip_before_action :verify_authenticity_token
 
     PUBLIC = Rails.root.join('public')
 
     def schedule_text
+        account_sid = ENV['TWILIO_ACCOUNT_SID']
+        auth_token = ENV['TWILIO_AUTH_TOKEN']
+    
+        @client = Twilio::REST::Client.new(account_sid, auth_token)
+
         if !params.has_key? :number or !params.has_key? :serverport or !params.has_key? :message
             head :invalid
             return
@@ -20,25 +22,18 @@ class TextingController < ApplicationController
         # make twilio call
         # if success return that
 
-
-
         message = @client.messages.create(
-                body: 'Hi there!',
-                from: '+15017122661',
-                to: '+15558675310'
-            )
+            body: params[:message],
+            from: '+17344363993',
+            to: params[:number]
+        )
             
         render :json => {}
    end
 
 
     def receive_response
-        if !params.has_key? :information or !params.has_key? :person or !params.has_key? :sincetime
-            head :invalid
-            return
-        end
-        
-        
+        puts request
         render :json => {}
     end
 end
