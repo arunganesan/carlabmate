ab9a58f11dabbcd9e0e0589b562a0bb179ff3c10
Arun <arunganesan123@gmail.com>
2019-11-22 08:00:07 -0500

    .

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@
 ( cd sandbox/green-gps/linkserver && ./bin/rails s -b 0.0.0.0 -p 8080 & )
 
 
-( cd library/react/libcarlab && npm run build )
-( cd library/react/user-input && npm run build )
+#( cd library/react/libcarlab && npm run build )
+#( cd library/react/user-input && npm run build )
 ( cd sandbox/green-gps/react && npm start & )
